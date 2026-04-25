570467fe9209e122e985a610b0628dd007068312
Yongxiang Liang <tanekliang@gmail.com>
2026-04-26 01:42:25 +0800

    [example/template] add fake-soc usart smoke test

--- example/template/demo/qemu_hello/qemu_hello.c
+++ example/template/demo/qemu_hello/qemu_hello.c
@@ -41,6 +41,53 @@ static void __qemu_uart_puts(const char *str)
     }
 }
 
+#if defined(__QEMU_FAKE_SOC__) && (VSF_HAL_USE_USART == ENABLED)
+static void __qemu_fake_soc_usart_puts(const char *str)
+{
+    vsf_usart_t *usart = (vsf_usart_t *)&vsf_hw_usart0;
+
+    while (*str != '\0') {
+        uint8_t ch = (uint8_t)*str++;
+
+        if (ch == '\n') {
+            uint8_t cr = '\r';
+            while (vsf_usart_txfifo_write(usart, &cr, 1) != 1) {
+            }
+        }
+        while (vsf_usart_txfifo_write(usart, &ch, 1) != 1) {
+        }
+    }
+}
+
+static void __qemu_fake_soc_usart_smoke_test(void)
+{
+    vsf_usart_t *usart = (vsf_usart_t *)&vsf_hw_usart0;
+    vsf_usart_cfg_t cfg = {
+        .mode = VSF_USART_NO_PARITY
+              | VSF_USART_1_STOPBIT
+              | VSF_USART_8_BIT_LENGTH
+              | VSF_USART_NO_HWCONTROL
+              | VSF_USART_TX_ENABLE
+              | VSF_USART_RX_ENABLE
+              | VSF_USART_SYNC_CLOCK_DISABLE
+              | VSF_USART_HALF_DUPLEX_DISABLE
+              | VSF_USART_TX_FIFO_THRESHOLD_EMPTY
+              | VSF_USART_RX_FIFO_THRESHOLD_NOT_EMPTY,
+        .baudrate = 38400,
+    };
+
+    if (VSF_ERR_NONE != vsf_usart_init(usart, &cfg)) {
+        __qemu_uart_puts("qemu fake soc usart init failed\n");
+        return;
+    }
+    if (fsm_rt_cpl != vsf_usart_enable(usart)) {
+        __qemu_uart_puts("qemu fake soc usart enable failed\n");
+        return;
+    }
+    __qemu_fake_soc_usart_puts("qemu fake soc usart polling ok\n");
+}
+#endif
+
 int main(void)
 {
     vsf_driver_init();
@@ -52,6 +99,10 @@ int main(void)
     __qemu_uart_puts("qemu fake soc hello world\n");
 #else
     __qemu_uart_puts("qemu hello world\n");
+#endif
+
+#if defined(__QEMU_FAKE_SOC__) && (VSF_HAL_USE_USART == ENABLED)
+    __qemu_fake_soc_usart_smoke_test();
 #endif
 
     while (1) {
